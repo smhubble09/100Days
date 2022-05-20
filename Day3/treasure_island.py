00a6495283b495f39fb7bf9983748fc192b21514
shane.hubble <smhubble09@gmail.com>
2022-05-20 10:46:46 -0600

    commit day 3

--- Day3/treasure_island.py
+++ Day3/treasure_island.py
@@ -27,12 +27,12 @@ print("Your mission is to find the treasure.")
 
 #Write your code below this line 👇
 
-choice1 = input("\nYou're at a crossroad. Where do you want to go? Type 'left' or 'right'\n")
+choice1 = input('\nYou\'re at a crossroad. Where do you want to go? Type "left" or "right"\n').lower()
 
 if choice1 == "left":
-    choice2 = input("You've come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across.\n")
+    choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.\n').lower()
     if choice2 == "wait":
-        choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color do you choose?\n")
+        choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color do you choose?\n").lower()
         if choice3 == "red":
             print("It's a room full of fire. Game Over.")
         elif choice3 == "yellow":
