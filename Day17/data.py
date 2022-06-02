efacdc5ce09467e6b0ba784a29e0a35009acb077
shane.hubble <smhubble09@gmail.com>
2022-06-02 12:23:37 -0600

    commit day 17

---
+++ Day17/data.py
@@ -0,0 +1,152 @@
+question_data = [
+        {
+            "category": "Computers",
+            "type": "boolean",
+            "difficulty": "medium",
+            "question": "The HTML5 standard was published in 2014.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Computers",
+            "type": "boolean",
+            "difficulty": "medium",
+            "question": "FLAC stands for 'Free Lossless Audio Condenser'.",
+            "correct_answer": "False",
+        },
+        {
+            "category": "Computers",
+            "type": "boolean",
+            "difficulty": "medium",
+            "question": "The very first recorded computer 'bug' was a moth found inside a Harvard Mark II computer.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Computers",
+            "type": "boolean",
+            "difficulty": "medium",
+            "question": "All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.",
+            "correct_answer": "False",
+        },
+        {
+            "category": "Computers",
+            "type": "boolean",
+            "difficulty": "medium",
+            "question": "'Windows NT' is a monolithic kernel.",
+            "correct_answer": "False",
+        },
+        {
+            "category": "Computers",
+            "type": "boolean",
+            "difficulty": "medium",
+            "question": "The open source program Redis is a relational database server.",
+            "correct_answer": "False",
+        },
+        {
+            "category": "Computers",
+            "type": "boolean",
+            "difficulty": "medium",
+            "question": "AMD created the first consumer 64-bit processor.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Computers",
+            "type": "boolean",
+            "difficulty": "medium",
+            "question": "Early RAM was directly seated onto the motherboard and could not be easily removed.",
+            "correct_answer": "True",
+        },
+        {
+            "category": "Computers",
+            "type": "boolean",
+            "difficulty": "medium",
+            "question": "The first dual-core CPU was the Intel Pentium D.",
+            "correct_answer": "False",
+        },
+        {
+            "category": "Computers",
+            "type": "boolean",
+            "difficulty": "medium",
+            "question": "The last Windows operating system to be based on the Windows 9x kernel was Windows 98.",
+            "correct_answer": "False",
+        },    
+        {
+            "category": "Animals",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "The Axolotl is an amphibian that can spend its whole life in a larval state.",
+            "correct_answer": "True",
+            
+        },
+        {
+            "category": "Animals",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "Kangaroos keep food in their pouches next to their children.",
+            "correct_answer": "False",
+            
+        },
+        {
+            "category": "Animals",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "A bear does NOT defecate during hibernation. ",
+            "correct_answer": "True",
+            
+        },
+        {
+            "category": "Animals",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "In 2016, the IUCN reclassified the status of Giant Pandas from endangered to vulnerable.",
+            "correct_answer": "True",
+            
+        },
+        {
+            "category": "Animals",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "Rabbits are rodents.",
+            "correct_answer": "False",
+            
+        },
+        {
+            "category": "Animals",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "A flock of crows is known as a homicide.",
+            "correct_answer": "False",
+            
+        },
+        {
+            "category": "Animals",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "Cats have whiskers under their legs.",
+            "correct_answer": "True",
+            
+        },
+        {
+            "category": "Animals",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "Rabbits can see what&#039;s behind themselves without turning their heads.",
+            "correct_answer": "True",
+            
+        },
+        {
+            "category": "Animals",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "The internet browser Firefox is named after the Red Panda.",
+            "correct_answer": "True",
+            
+        },
+        {
+            "category": "Animals",
+            "type": "boolean",
+            "difficulty": "easy",
+            "question": "The freshwater amphibian, the Axolotl, can regrow it&#039;s limbs.",
+            "correct_answer": "True",
+            
+        }
+]
