28293ee74c8d62062a75ca9ec59ccba5f6db0e03
smhubble09 <49952057+smhubble09@users.noreply.github.com>
2022-06-23 09:51:53 -0600

    Delete Day40 directory

--- Day40/main.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-USERNAME = "d7857d475b7faf250d8f5d7ee6b66e8a"
-PROJECT_NAME = "flightDeals"
-SHEET_NAME = "users"
-SHEETY_ENDPOINT = f"https://api.sheety.co/{USERNAME}/{PROJECT_NAME}/{SHEET_NAME}"
-
