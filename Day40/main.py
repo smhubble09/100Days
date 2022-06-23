4b5e911696fac65d10b6668d635cad7ac53115fd
shane.hubble <smhubble09@gmail.com>
2022-06-23 09:32:26 -0600

    commit day 39

---
+++ Day40/main.py
@@ -0,0 +1,7 @@
+import requests
+
+USERNAME = "d7857d475b7faf250d8f5d7ee6b66e8a"
+PROJECT_NAME = "flightDeals"
+SHEET_NAME = "users"
+SHEETY_ENDPOINT = f"https://api.sheety.co/{USERNAME}/{PROJECT_NAME}/{SHEET_NAME}"
+
