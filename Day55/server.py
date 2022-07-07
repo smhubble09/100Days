3d305cd5a548844c89dcb96dece2616d50a01b0a
shane.hubble <smhubble09@gmail.com>
2022-07-07 09:19:41 -0600

    fixed h1 color

--- Day55/server.py
+++ Day55/server.py
@@ -12,13 +12,13 @@ def home():
 @app.route('/<int:number>')
 def number_guess(number):
     if number > random_num:
-        return '<h1 style="color=red">Too high, try again!</h1>'\
+        return '<h1 style="color:red">Too high, try again!</h1>'\
             '<img src="https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif" alt="Too High">'
     elif number < random_num:
-        return '<h1 style="color=purple">Too low, try again!</h1>'\
+        return '<h1 style="color:purple">Too low, try again!</h1>'\
             '<img src="https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif" alt="Too Low">'
     else:
-        return '<h1 style="color=green">You found me!</h1>'\
+        return '<h1 style="color:green">You found me!</h1>'\
             '<img src="https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif" alt="Correct">'
 
 if __name__ == "__main__":
